a2a6da97ba535034f0b25017d22d8783e544cab8
404 <daehyeon7902@gmail.com>
2025-03-24 17:39:01 +0900

    v1

--- week3.py
+++ week3.py
@@ -1,3 +1,8 @@
 l = [99,8,-7,8,16]
 for i in range(len(l)):
     print(f"{l[i]:3} {id(l[i])}")
+
+groups = ['hot','seventeen','blackpink','njz']
+ratings = [1,2,3,4]
+group_rating = dict(list(zip(groups,ratings)))
+print(group_rating)
