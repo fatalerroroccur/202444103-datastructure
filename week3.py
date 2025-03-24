c84e66ef0b03ba9100cda61fe5ba3429b1343ee4
404 <daehyeon7902@gmail.com>
2025-03-24 18:08:25 +0900

    asdf

--- week3.py
+++ week3.py
@@ -6,3 +6,30 @@ groups = ['hot','seventeen','blackpink','njz']
 ratings = [1,2,3,4]
 group_rating = dict(list(zip(groups,ratings)))
 print(group_rating)
+
+
+#def duplicate_city(cities):
+#    result_city = []
+#    s=set()
+#    for city in cities:
+#        l1 = len(s)
+#        s.add(city)
+#        l2 = len(s)
+#        if l1 == l2:
+#            result_city.append(city)
+#        return result_city
+#cities = ['incheon','seoul','incheon','incheon','gwangju']
+#cities = set(cities)
+#cities.add('incheon')
+#print(cities)
+#print(set(duplicate_city(cities)))
+
+def inters(l3,l4):
+    l5 = []
+        for v in l3:
+            if v in l4:
+                l5.append(v)
+        return l5
+    l3 = [45,5,22,31,7,19]
+    l4 = [22,1,5,2,27,27,19,23,13,41]
+    print(inters(l3,l4))
