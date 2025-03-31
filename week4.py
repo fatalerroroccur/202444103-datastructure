1a6e7f149597e6418c52dbaa82fc124d343a9958
404 <daehyeon7902@gmail.com>
2025-03-31 18:03:40 +0900

    asdfa

--- week4.py
+++ week4.py
@@ -1,20 +1,48 @@
+import random
+
 class Node:
     def __init__(self, data, link=None):
         self.data = data
         self.link = link
+
+
 class LinkedList:
     def __init__(self):
         self.head = None
-    def append(self,data):
+
+
+    def append(self, data):
         if not self.head:
             self.head = Node(data)
             return
         current = self.head
         while current.link:
-            current = current.link
+            current = current.link  # move current
         current.link = Node(data)
 
+
+    # def is_find(self, target):
+    def search(self, target):
+        current = self.head
+        while current.link:
+            if target == current.data:
+                return f"{target}을(를) 찾았습니다!"
+            else:
+                current = current.link
+        return  f"{target}은(는) 링크드 리스트 안에 존재하지 않습니다."
+
+
+    def __str__(self):
+        current = self.head
+        result = ""
+        while current is not None:
+            result = result + f"{current.data} -> "
+            current = current.link
+        return result + "END"
+
+
 ll = LinkedList()
-ll.append(8)
-ll.append(10)
-ll.append(-9)
+for _ in range(10):
+    ll.append(random.randint(1, 20))
+print(ll)
+print(ll.search(10))
